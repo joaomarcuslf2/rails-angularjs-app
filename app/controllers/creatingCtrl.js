0a213930c4d5d090dcbe12413ae7e36378030c7d
João Marcus de Lemos Fernandes <jmldesignergrafico@gmail.com>
2016-09-11 13:28:38 -0300

    Added button valiidation

--- app/controllers/creatingCtrl.js
+++ app/controllers/creatingCtrl.js
@@ -3,6 +3,8 @@ angular
 	.controller('creatingCtrl', ['$scope', 'messagesService', '$location', function ($scope, $messagesService, $location) {
 		$scope.sendingMessage = false;
 		$scope.sendMessage = function(post) {
+			$scope.sendingMessage = true;
+			
 			$messagesService.send(post, function(data) {
 				$location.path('/list')
 			});
